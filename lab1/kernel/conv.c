825ca17f4359f9d5e9b4cb3aa3eece4b8dc8aadd
Abiola Bolaji <ab3394@slurm-instructional-login.ece.cornell.edu>
2026-09-25 12:46:46 -0400

    f

---
+++ lab1/kernel/conv.c
@@ -0,0 +1,7 @@
+#include "conv.h"
+
+// Basic convolution operation
+float ***convolution(float ***image, int numChannels, float ****kernel, float *biasData, int numFilters, int inputSize, int kernelSize)
+{
+    // TODO: Implement the convolution operation
+}
---
+++ lab1/kernel/matrix_ops.c
@@ -0,0 +1,6 @@
+#include "matrix_ops.h"
+
+float **matmul(float **A, float **B, int A_rows, int A_cols, int B_rows, int B_cols) 
+{
+    // TODO: Implement matrix multiplication
+}
